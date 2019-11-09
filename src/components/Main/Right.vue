f1375719dfd388d88f5cf39ec118671989ce0078
kenosej <kenankenosej@gmail.com>
2019-11-09 21:10:18 +0100

    Added manual

--- src/components/Main/Right.vue
+++ src/components/Main/Right.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="right"></div>
+  <div class="right">
+    <div class="manual">
+      <h1>Manual</h1>
+      <span>Grab your tickets before the timer hits 00:00.</span>
+      <span>On your ticket, you will see</span>
+      <span>combination of 10 numbers and ID of your ticket.</span>
+      <span>You can't make new tickets when the timer hits 00:00.</span>
+      <span>New timer arrives and every 10 seconds new number will be generated.</span>
+      <span>Our bingo will create a combination of 8 different numbers.</span>
+      <span>On screen you will see which ticket has the highest hits.</span>
+      <span>Ticket with most hits is a winner.</span>
+      <span>If you are lucky enough and you guess all of eight numbers, you scored Bingo.</span>
+      <span>Good luck!</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +27,18 @@ export default {
   grid-area: right;
   border: 1px solid chartreuse;
 }
+
+  .manual {
+    margin: 10% 5%;
+    line-height: 1.6;
+  }
+
+  .manual h1 {
+    margin-bottom: 5px;
+  }
+
+  .manual span {
+    display: block;
+    font-size: 16px;
+  }
 </style>
