0abea746f48b8468bf0ae332dbc266303abf146f
edin.sahbaz <edin.sahbaz@hotmail.com>
2019-11-09 22:49:59 +0100

    Ending view prep

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="main">
-        <Left />
-        <Ending/>
-<!--        <middleUpper />-->
-        <middleBottom />
-        <Right />
-    </div>
+  <div class="main">
+    <Left />
+    <!--<Ending/>-->
+    <middleUpper />
+    <middleBottom />
+    <Right />
+  </div>
 </template>
 
 <script>
-    import Left from "../components/Main/Left";
-    // import middleUpper from "../components/Main/middleUpper";
-    import middleBottom from "../components/Main/middleBottom";
-    import Right from "../components/Main/Right";
-    import Ending from "../components/Main/Ending";
+import Left from "../components/Main/Left";
+import middleUpper from "../components/Main/middleUpper";
+import middleBottom from "../components/Main/middleBottom";
+import Right from "../components/Main/Right";
+//import Ending from "../components/Main/Ending";
 
-    export default {
-        name: "Main",
-        components: {
-            Left,
-            // middleUpper,
-            middleBottom,
-            Right,
-            Ending
-        }
-    }
+export default {
+  name: "Main",
+  components: {
+    Left,
+    middleUpper,
+    middleBottom,
+    Right
+    //Ending
+  }
+};
 </script>
 
 <style scoped>
-    .main {
-        display: grid;
-        grid-template-rows: 65vh 35vh;
-        grid-template-columns: 20% 60% 20%;
-        grid-template-areas:
-                "left middleUpper right"
-                "left middleBottom right";
-        color: #f8f0ee;
-        background: #2d3440;
-    }
+.main {
+  display: grid;
+  grid-template-rows: 65vh 35vh;
+  grid-template-columns: 20% 60% 20%;
+  grid-template-areas:
+    "left middleUpper right"
+    "left middleBottom right";
+  color: #f8f0ee;
+  background: #2d3440;
+}
 </style>
