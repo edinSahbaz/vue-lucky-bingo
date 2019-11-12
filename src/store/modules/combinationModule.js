1247106b6f2af3bc375e5931194cd10568c9db61
kenosej <kenankenosej@gmail.com>
2019-11-12 22:52:46 +0100

    State management moved to Left.vue. Older version of state management through new tab view wasn't sustainable

--- src/store/modules/combinationModule.js
+++ src/store/modules/combinationModule.js
@@ -10,7 +10,7 @@ const getters = {
     get_lastNum: state => state.lastNum,
     get_Combination: state => state.combination,
     get_userCombinations: state => state.userCombinations,
-    get_TicketId: state => state.tempId
+    get_ticketId: state => state.tempId
 };
 
 const actions = {
@@ -39,11 +39,9 @@ const actions = {
     },
 
     // user comb
-    incrementToMutation({commit}) {
-              commit('increment');
-    },
+    generateUserCombination({ commit }) {
+        commit("increment");
 
-    generateUserCombination() {
         state.userCombinations.push(
             {
                 id: state.tempId,
@@ -51,42 +49,26 @@ const actions = {
             }
         );
 
-        // Šaljem oblik u userCombination kao ovo gore
-      // u nizu se redaju objekti s ovim gore propertijima
-      // U ticket dostavljam komplet objekat
-      // Moglo se napravit da uzima zadnju vrijednost ID iz
-      // userCombination, al onda ćemo zaobić mutacije
-      // i nećemo ono na naučit
-      // s ovim dole cirkulišem kroz userCombinations i regenerišem
-      // random kombinaciju, koja će bit dostupna u ticket.vue
-      // a ipak će ostat u combinationModule.js KOJA nam može bit središnji
-      // store za sve kombinacije..
-      // eto, poderi kroz mutacije.. Bilo bi fino
-      // piso sam što prostije.. da ga uradimo prvo prosto
-      // pa ćemo se onda preseravat :D
-      // Poy, sretno
-      // (opet iskomentariši ko prošli put, bilo je top <3)
-
         state.userCombinations.forEach((e) => {
             if (e.id === state.tempId) {
-              for (let i = 0; i < 8; i++) {
-                let randNum = Math.floor(Math.random() * 100);
+                for (let i = 0; i < 8; i++) {
+                    let randNum = Math.floor(Math.random() * 100);
 
-                if (!e.comb.includes(randNum)) {
-                  e.comb[i] = randNum
-                } else {
-                  i--;
+                    if (!e.comb.includes(randNum)) {
+                        e.comb[i] = randNum
+                    } else {
+                        i--;
+                    }
                 }
-              }
             }
         });
     }
 };
 
 const mutations = {
-  increment (state) {
-    state.tempId++;
-  }
+    increment(state) {
+        state.tempId++;
+    }
 };
 
 export default {
--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -1,77 +1,77 @@
 <template>
-  <div class="ticket">
-    <p>
-      Ticket ID:
-      <strong>#{{get_TicketId}}</strong>
-      <br />These are the numbers that have been generated for you:
-    </p>
-    <div class="balls">
-      <Ball :num="combination[combination.length - 1]['comb'][0]" />
-      <Ball :num="combination[combination.length - 1]['comb'][1]" />
-      <Ball :num="combination[combination.length - 1]['comb'][2]" />
-      <Ball :num="combination[combination.length - 1]['comb'][3]" />
-      <Ball :num="combination[combination.length - 1]['comb'][4]" />
-      <Ball :num="combination[combination.length - 1]['comb'][5]" />
-      <Ball :num="combination[combination.length - 1]['comb'][6]" />
-      <Ball :num="combination[combination.length - 1]['comb'][7]" />
+    <div class="ticket">
+        <p>
+            Ticket ID:
+            <strong>#{{id}}</strong>
+            <br/>These are the numbers that have been generated for you:
+        </p>
+        <div class="balls">
+            <Ball :num="combination[0]"/>
+            <Ball :num="combination[1]"/>
+            <Ball :num="combination[2]"/>
+            <Ball :num="combination[3]"/>
+            <Ball :num="combination[4]"/>
+            <Ball :num="combination[5]"/>
+            <Ball :num="combination[6]"/>
+            <Ball :num="combination[7]"/>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Ball from "./../components/Main/Elements/Ball.vue";
-import { mapGetters, mapActions } from "vuex";
+    import Ball from "./../components/Main/Elements/Ball.vue";
 
-// Ne prepoznaje mutaciju, ne želi ugrabit generalnu vrijednost mutacije
-// a tempId treba da bude odvojen od logike i da se povećava svaki put kad
-// se klikne dugme
-// Moro sam promjenit u Left.vue da poziva akciju iz combinationModule jer druagčije
-// nemoš promjenit state kroz mutaciju
-// Ticket ne prepoznaje mutaciju, to bi bilo fino sredit
-// Ja nisam jeo od škole, jbg, riješio bih al et vrijeme jebe
+    export default {
+        name: "Ticket",
+        components: {
+            Ball
+        },
+        data() {
+            return {
+                id: "",
+                combination: []
+            };
+        },
+        methods: {
+            getParameterByName(name, url) {
+                if (!url) url = window.location.href;
+                name = name.replace(/[[\]]/g, '\\$&');
+                var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                    results = regex.exec(url);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return decodeURIComponent(results[2].replace(/\+/g, ' '));
+            }
+        },
+        created() {
+            this.id = this.getParameterByName('id', location.href);
 
-export default {
-  name: "Ticket",
-  computed: {
-    ...mapGetters(["get_userCombinations", "get_TicketId"])
-  },
-  components: {
-    Ball
-  },
-  data() {
-    return {
-      combination: {}
+            for (let i = 1; i <= 8; i++) { // good code is poem
+                this.combination.push(this.getParameterByName(`num${i}`, location.href))
+            }
+        }
     };
-  },
-  methods: {
-    ...mapActions(["generateUserCombination"])
-  },
-  created() {
-    this.generateUserCombination();
-    this.combination = this.get_userCombinations;
-  }
-};
 </script>
 
 <style scoped>
-p {
-  margin-top: 0.5em;
-  margin-left: 1em;
-  color: #ef424c;
-  font-size: 1.4em;
-}
+    p {
+        margin-top: 0.5em;
+        margin-left: 1em;
+        color: #ef424c;
+        font-size: 1.4em;
+    }
 
-.ticket {
-  margin: 20px;
-  border-radius: 30px;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  background: #f8f0ee;
-}
+    .ticket {
+        margin: 20px;
+        border-radius: 30px;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        background: #f8f0ee;
+    }
 
-.balls {
-  display: flex;
-  flex-direction: row;
-}
+    .balls {
+        display: flex;
+        flex-direction: row;
+    }
 </style>
