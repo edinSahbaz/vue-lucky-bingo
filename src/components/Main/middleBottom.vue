1ce1c796895a8bb59986a1fd39cdb935e0f601d3
kenosej <kenankenosej@gmail.com>
2019-11-13 00:39:42 +0100

    (Added) Declare a winner with highest correct combinations; (Added) Show winners combination in middleBottom

--- src/components/Main/middleBottom.vue
+++ src/components/Main/middleBottom.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="middleBottom">
-    <Ball />
-    <Ball />
-    <Ball />
-    <Ball />
-    <Ball />
-    <Ball />
-    <Ball />
-    <Ball />
+    <Ball :num="get_winnerCombination['winnerComb'][0]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][1]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][2]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][3]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][4]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][5]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][6]"/>
+    <Ball :num="get_winnerCombination['winnerComb'][7]"/>
   </div>
 </template>
 
 <script>
 import Ball from "./Elements/Ball.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "middleBottom",
+  computed: {
+    ...mapGetters(['get_winnerCombination'])
+  },
   components: {
     Ball
   }
--- src/store/modules/combinationModule.js
+++ src/store/modules/combinationModule.js
@@ -3,14 +3,19 @@ const state = {
     combination: [],
     userCombinations: [],
     tempId: 0,
-    tickets: []
+    tickets: [],
+    winnerCombination: {
+        id: "",
+        winnerComb: []
+    }
 };
 
 const getters = {
     get_lastNum: state => state.lastNum,
     get_Combination: state => state.combination,
     get_userCombinations: state => state.userCombinations,
-    get_ticketId: state => state.tempId
+    get_ticketId: state => state.tempId,
+    get_winnerCombination: state => state.winnerCombination
 };
 
 const actions = {
@@ -28,12 +33,14 @@ const actions = {
     },
     showNum() {
         let i = 0;
-        setInterval(() => {
+
+        let ntrvl = setInterval(() => {
             if (i < 8) {
                 state.lastNum = state.combination[i];
                 i++;
             } else {
-                clearInterval();
+                clearInterval(ntrvl);
+                this.findWinner();
             }
         }, 6000);
     },
@@ -62,6 +69,52 @@ const actions = {
                 }
             }
         });
+    },
+
+    /*
+    findWinner se aktivira nakon 6 sekundi nakon što se zaustavi tajmer na
+    velkoj loptici, tj na krajnjem intervalu
+    ti si počeo radit statemanagement i dodijeljivanje id i komb u Ticket.vue
+    sto je u novom tabu i zbog toga ne moze pristupit ni globalnim varijablama
+    ni mutacijama.. sve racuna pod resetom
+    to je bio problem
+    sve sam prebacio u Left.vue gdje šaljem preko GET sve info
+    što je jedini način da komuniciram s novim tabom i pritom sve podatke
+    o ID i kombinaciji sacuvam
+    6 sekundi nakon kraja rokne ovu funkciju, prođe kroz sve userCombinations
+    i upoređuje ko ima najviše tačnih kombinacija
+    na kraju u middleBottom izvučem dobitnu kombinaciju
+    (tačne brojeve možemo npr obojit drugom bojom)
+    sad TODO:
+    prebacit na onaj Ending screen sa infom kad se sve zavrsi
+    onemogucit button na leftu nakon odredjenog vremena
+    i ono npr pastavit kad pačne izvlačit brojeve da vrti i tu tajmere od po 5 sekundi
+    i onda cemo se jebavat s animacijama i doradjivanjem
+    ovo je suuuper :D (jbt kruh i novi tab, jedva sam se izvuko iz govana hahahaha)
+    */
+
+    findWinner() {
+        state.userCombinations.forEach(e => {
+            e.correctCombs = 0;
+            e.comb.forEach(combItem => {
+                if (state.combination.includes(combItem)) {
+                    e.correctCombs++;
+                }
+            })
+        });
+
+        let highestComb = -Infinity;
+        let indexOfHighestComb = "";
+
+        state.userCombinations.forEach((e, index) => {
+            if (e.correctCombs > highestComb) {
+                highestComb = e.correctCombs;
+                indexOfHighestComb = index;
+            }
+        });
+
+        state.winnerCombination.id = state.userCombinations[indexOfHighestComb].id;
+        state.winnerCombination["winnerComb"] = state.userCombinations[indexOfHighestComb].comb;
     }
 };
 
